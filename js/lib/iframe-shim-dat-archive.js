c04e52a9b9b0a7e57b7e370d9b77b12dd3ee3594
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2019-08-18 01:29:37 +0200

    Add missing DatArchiveShim methods

--- js/lib/iframe-shim-dat-archive.js
+++ js/lib/iframe-shim-dat-archive.js
@@ -1,15 +1,27 @@
+const STATIC = /^static\:/
+
 export function shimDatArchiveGlobal () {
   window.DatArchive = DatArchiveShim
 }
 
-export function handleDatArchiveRequests () {
+export function handleDatArchiveRequests (allow = () => true) {
   window.addEventListener('message', async function ({data, source}) {
     if (data.type !== 'archive:req') return
-    let dat = new DatArchive(data.url)
-    let fn = dat[data.method].bind(dat)
+
+    if (!allow(data.url, data.method)) {
+      throw new Error('DatArchive access refused')
+    }
+    let method
+
+    if (STATIC.test(data.method)) {
+      method = DatArchive[data.method.replace(STATIC, '')]
+    } else {
+      let dat = new DatArchive(data.url)
+      method = dat[data.method].bind(dat)
+    }
 
     try {
-      let content = await fn(...data.args)
+      let content = await method(...data.args)
       respond({type: 'archive:res', content})
     } catch (err) {
       let {name, message, stack} = err
@@ -28,25 +40,106 @@ export function handleDatArchiveRequests () {
  * Shim interface
  */
 class DatArchiveShim {
+  static async load (...args) {
+    let res = await request({method: 'static:load', args})
+    return new this(res.url)
+  }
+
+  static async create (...args) {
+    let res = await request({method: 'static:create', args})
+    return new this(res.url)
+  }
+
+  static async fork (...args) {
+    let res = await request({method: 'static:fork', args})
+    return new this(res.url)
+  }
+
+  static async unlink (...args) {
+    return request({method: 'static:unlink', args})
+  }
+
+  static async selectArchive (...args) {
+    let res = await request({method: 'static:selectArchive', args})
+    return new this(res.url)
+  }
+
+  static async resolveName (...args) {
+    return request({method: 'static:resolveName', args})
+  }
+
   constructor (url) {
     this.url = url
   }
 
-  async readFile (...args) {
-    return request({type: 'archive:req', method: 'readFile', url: this.url, args})
+  async getInfo (...args) {
+    return request({method: 'getInfo', url: this.url, args})
+  }
+
+  async configure (...args) {
+    return request({method: 'configure', url: this.url, args})
   }
 
   async stat (...args) {
-    let stat = await request({type: 'archive:req', method: 'stat', url: this.url, args})
+    let stat = await request({method: 'stat', url: this.url, args})
     stat.isDirectory = () => stat.mode === 16877
     stat.isFile = () => stat.mode === 33188
     return stat
+  }
+
+  async readFile (...args) {
+    return request({method: 'readFile', url: this.url, args})
+  }
+
+  async readdir (...args) {
+    return request({method: 'readdir', url: this.url, args})
+  }
+
+  async writeFile (...args) {
+    return request({method: 'writeFile', url: this.url, args})
+  }
+
+  async mkdir (...args) {
+    return request({method: 'mkdir', url: this.url, args})
+  }
+
+  async unlink (...args) {
+    return request({method: 'unlink', url: this.url, args})
+  }
+
+  async rmdir (...args) {
+    return request({method: 'rmdir', url: this.url, args})
+  }
+
+  async copy (...args) {
+    return request({method: 'copy', url: this.url, args})
+  }
+
+  async rename (...args) {
+    return request({method: 'rename', url: this.url, args})
+  }
+
+  async history (...args) {
+    return request({method: 'history', url: this.url, args})
+  }
+
+  checkout (version) {
+    let urlp = new URL(this.url)
+    let base = urlp.hostname.split('+')[0]
+    this.url = `dat://${base}+${version}`
+    return this
+  }
+
+  async download (...args) {
+    return request({method: 'download', url: this.url, args})
   }
 }
 
 function request (params) {
   return new Promise(function (resolve, reject) {
     params.token = Math.random()
+    params.type = 'archive:req'
+
     window.addEventListener('message', listen)
     window.top.postMessage(params, window.location.origin)
 
